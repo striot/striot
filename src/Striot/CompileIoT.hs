05a6fad753c09ddd841da371e85ff539dd9d6089
Jonathan Dowland <jon@dow.land>
2018-01-31 18:58:26 +0000

    output.hs: test tool for invoking CompileIoT
    output.hs: simple program to write out a series of .hs files for
    the strings generated by CompileIoT. Specifically partitioning the
    "s4" test definition, which roughly corresponds to examples/pipeline
    
    CompileIoT.hs: Adjust s4 to add a sink node, which is required (avoids a
    runtime error when you try to combine generateCode and s4)
    
    Note: the s4 test definition in CompileIoT is duplicated into output.hs,
    rather than exported.

--- src/Striot/CompileIoT.hs
+++ src/Striot/CompileIoT.hs
@@ -399,12 +399,13 @@ ex415p2 = StreamGraph "p2" 7 [(6,"String")] [StreamOperation 7 [6] Sink ["print"
 ex415 = graphPartitionCategory s1 ex415p2
 
 s4:: StreamGraph
-s4 = StreamGraph "pipeline" 5 [] [
+s4 = StreamGraph "pipeline" 6 [] [
        StreamOperation 1 [ ] Source    ["src1"]                                                                                "Stream String"          ["SourceFileContainingsrc1.hs"],
        StreamOperation 2 [1] Map       ["\\st-> st++st"]                                                                       "Stream String"          [],
        StreamOperation 3 [2] Map       ["\\st-> reverse st"]                                                                   "Stream String"          [],
        StreamOperation 4 [3] Map       ["\\st-> \"Incoming Message at Server: \" ++ st"]                                       "Stream String"          [],
-       StreamOperation 5 [4] Window    ["(chop 2)"]                                                                            "Stream [String]"        []]
+       StreamOperation 5 [4] Window    ["(chop 2)"]                                                                            "Stream [String]"        [],
+       StreamOperation 6 [5] Sink      ["print"]                                                                               ""                       []]
 
 s4parts = generateCode s4 [(1,[1,2]),(2,[3]),(3,[4,5])] stdImports
 exs41 = putStrLn $ s4parts!!0
