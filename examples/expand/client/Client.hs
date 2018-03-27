082ba336632b02b58782ec99bbd81e591bb701da
Jonathan Dowland <jon@dow.land>
2018-03-27 14:19:20 +0100

    stick to camelCase for names

--- examples/expand/client/Client.hs
+++ examples/expand/client/Client.hs
@@ -15,19 +15,19 @@ src :: IO String
 src = do
     indices <- replicateM 10 (getStdRandom (randomR (0,39)) :: IO Int)
 
-    let s = unwords $ map (random_words !!) indices in do
+    let s = unwords $ map (randomWords !!) indices in do
         threadDelay 1000000
         return s
 
 streamGraph :: Stream String -> Stream [String]
-streamGraph = streamMap get_hashtags
+streamGraph = streamMap getHashtags
 
-get_hashtags :: String -> [String]
-get_hashtags s = filter (('#'==).head) (words s)
+getHashtags :: String -> [String]
+getHashtags s = filter (('#'==).head) (words s)
 
 -- 40 words picked randomly from the dictionary, 20 of which are prefixed
 -- with # to simulate hashtags
-random_words = words "Angelica #Seine #sharpened sleeve consonance diabolically\
+randomWords = words "Angelica #Seine #sharpened sleeve consonance diabolically\
 \ #bedlam #sharpener sentimentalizing amperage #quilt Ahmed #quadriceps Mia\
 \ #burglaries constricted julienne #wavier #gnash #blowguns wiping somebodies\
 \ nematode metaphorical Chablis #taproom disrespects #oddly ideograph rotunda\
