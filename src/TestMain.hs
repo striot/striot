48b25c5d229ad0f07fff66de67e0af06773f8441
Jonathan Dowland <jon@dow.land>
2018-05-11 11:31:24 +0100

    hook VizGraph into test suite; improvements
    Add HTF boilerplate to VizGraph.hs (no tests as of yet)
    
    Move TestMain to reflect it importing things from src/ as well as src/Striot
    
    Import *all* Striot modules into TestMain, even if they do not define any
    tests. They will at least cause test suite failures if the modules themselves
    stop compiling.
    
    Update Cabal file with VizGraph package dependencies
    
    fixup

--- src/TestMain.hs
+++ src/TestMain.hs
@@ -4,5 +4,10 @@ module Main where
 
 import Test.Framework
 import {-@ HTF_TESTS @-} Striot.CompileIoT
+import {-@ HTF_TESTS @-} VizGraph
+
+import Striot.FunctionalIoTtypes
+import Striot.FunctionalProcessing
+import Striot.Nodes
 
 main = htfMain htf_importedTests
