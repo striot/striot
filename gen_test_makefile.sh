c3aeb8cec0ad83e8a549f089a3e6b374372686be
Jonathan Dowland <jon@dow.land>
2018-11-16 10:46:53 +0000

    gen_makefile.sh: build a makefile test harness
    A crude way of exercising most of the compiler parts of the tests/:
    gen_makefile.sh constructs a Makefile that builds every "generate"
    binary in tests/, and all node* binaries it can find too.

---
+++ gen_test_makefile.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -u
+set -e
+shopt -s nullglob
+
+# Generates a Makefile with a default target that performs the following steps
+# for every applicable example under examples/:
+#
+#   • build and run "generate" (clean first to ensure it's regenerated)
+#   • build any node?/node?.hs files output by "generate"
+#
+# This can be used as a brute-force method of ensuring that the above all
+# typecheck and compile properly when making structural changes to the Striot
+# library (and is in lieu of a better test suite)
+
+gen()
+{
+    a=()
+    echo default: default2
+    echo
+    for m in examples/*/generate.hs; do
+        d="$(dirname "$m")"
+        a+=("$d")
+
+        echo -e "$d:"
+        echo -e "\tmake -C $d clean"
+        echo -e "\tmake -C $d generate"
+        echo -e "\tcd \"$d\" && ./generate"
+
+        for n in "$d"/node?/node?.hs
+            do echo -e "\tghc $n"
+        done
+        echo
+    done
+    echo default2: "${a[@]}"
+    echo .PHONY: default default2 "${a[@]}"
+}
+
+gen
