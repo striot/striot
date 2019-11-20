d6b959eeecab9da4eb5b8460c5532912ef531aec
Adam Cattermole <a.d.cattermole@gmail.com>
2019-11-20 16:28:42 +0000

    Add Kafka pipeline example

---
+++ examples/pipeline-kafka/node1/node.hs
@@ -0,0 +1,20 @@
+-- node1
+import Striot.FunctionalIoTtypes
+import Striot.FunctionalProcessing
+import Striot.Nodes
+import Control.Concurrent
+
+
+src1 = do
+    threadDelay (1000*1000)
+    return "Hello from Client!"
+
+streamGraphFn :: Stream String -> Stream String
+streamGraphFn n1 = let
+    n2 = (\s -> streamMap (\st->st++st) s) n1
+    in n2
+
+
+main :: IO ()
+main = do
+       nodeSourceKafka src1 streamGraphFn "node1" "kafka" "9092"
