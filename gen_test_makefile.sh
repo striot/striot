28968a965b904ad7170e7522a4ce831416e4a123
Jonathan Dowland <jon@dow.land>
2020-02-10 21:41:47 +0000

    clarifying comment in gen_test_makefile.sh

--- gen_test_makefile.sh
+++ gen_test_makefile.sh
@@ -6,8 +6,12 @@ shopt -s nullglob
 # Generates a Makefile with a default target that performs the following steps
 # for every applicable example under examples/:
 #
-#   • build and run "generate" (clean first to ensure it's regenerated)
-#   • build any node?/node.hs files output by "generate"
+#  1. build and run "generate" (clean first to ensure it's regenerated)
+#  2. build any node?/node.hs files output by "generate"
+#
+# In a clean checkout, this script needs to be run *twice*: the first Makefile
+# to generate all the individual nodes, and the second run to generate rules 
+# to build all of them.
 #
 # This can be used as a brute-force method of ensuring that the above all
 # typecheck and compile properly when making structural changes to the Striot
