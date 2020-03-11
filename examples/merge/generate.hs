904c5ea9a40c24697fbcd432cb75e20eff008d79
Jonathan Dowland <jon@dow.land>
2020-03-11 21:38:01 +0000

    Simplify the "merge" example
    We no longer need the dummy Map operators that just apply "id",
    CompileIoT can now handle "operator-less" partitions.
    
    Make the two sources emit at different rates, so it is easier to
    see the semantics of the inter-partition merging.

--- examples/merge/generate.hs
+++ examples/merge/generate.hs
@@ -6,21 +6,19 @@ import Striot.CompileIoT
 import Striot.StreamGraph
 import Algebra.Graph
 
-source x = "do\n\
-\    threadDelay (1000*1000)\n\
+source x y = "do\n\
+\    threadDelay ("++y++")\n\
 \    putStrLn \"sending '"++x++"'\"\n\
 \    return \""++x++"\""
 
+v1 = StreamVertex 1 Source [source "foo" "1000*1000"] "String" "String"
+v2 = StreamVertex 2 Source [source "bar"  "500*1000"] "String" "String"
+v3 = StreamVertex 3 Merge  ["[s1,s2]"]                "String" "String"
+-- XXX: ^ we lie about the input type here, because the generated function has split-out arguments
+v4 = StreamVertex 4 Sink   ["mapM_ print"] "String" "IO ()"
 
-v1 = StreamVertex 1 Source [source "foo"]  "String" "String"
-v2 = StreamVertex 2 Map    ["id", "s"]     "String" "String"
-v3 = StreamVertex 3 Source [source "bar"]  "String" "String"
-v4 = StreamVertex 4 Map    ["id", "s"]     "String" "String"
-v5 = StreamVertex 5 Merge  ["[s1,s2]"]     "String" "String" -- XXX: we lie about the input type here, because the generated function has split-out arguments
-v6 = StreamVertex 6 Sink   ["mapM_ print"] "String" "IO ()"
+graph = overlay (path [v1, v3]) $ path [v2, v3, v4]
 
-graph = overlay (path [v3, v4, v5]) $ path [v1, v2, v5, v6]
-
-parts = [[1,2],[3,4],[5,6]]
+parts = [[1],[2],[3,4]]
 
 main = partitionGraph graph parts defaultOpts
