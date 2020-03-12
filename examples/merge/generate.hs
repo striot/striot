973e47dfd287cf6acb12fe8fe887eda38df0a4a0
Jonathan Dowland <jon@dow.land>
2020-03-12 09:15:42 +0000

    Expand 'merge' example to 3 producers
    This was not possible in the older scheme where we used specified
    "nodeSink" or "nodeLink" functions for multi-valenced Partitions,
    since we had only written 1 or 2-valence versions (nodeSink2).

--- examples/merge/generate.hs
+++ examples/merge/generate.hs
@@ -13,12 +13,14 @@ source x y = "do\n\
 
 v1 = StreamVertex 1 Source [source "foo" "1000*1000"] "String" "String"
 v2 = StreamVertex 2 Source [source "bar"  "500*1000"] "String" "String"
-v3 = StreamVertex 3 Merge  ["[s1,s2]"]                "String" "String"
+v3 = StreamVertex 3 Source [source "baz"  "200*1000"] "String" "String"
+
+v4 = StreamVertex 4 Merge [] "String" "String"
 -- XXX: ^ we lie about the input type here, because the generated function has split-out arguments
-v4 = StreamVertex 4 Sink   ["mapM_ print"] "String" "IO ()"
+v5 = StreamVertex 5 Sink ["mapM_ print"] "String" "IO ()"
 
-graph = overlay (path [v1, v3]) $ path [v2, v3, v4]
+graph = (overlays (map vertex [v1,v2,v3]) `connect` (vertex v4)) `overlay` path [v4,v5]
 
-parts = [[1],[2],[3,4]]
+parts = [[1],[2],[3],[4,5]]
 
 main = partitionGraph graph parts defaultOpts
