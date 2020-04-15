0117127aab6d4fc2d6f5721887e1156ef243f0f9
Jonathan Dowland <jon@dow.land>
2020-04-15 11:35:09 +0100

    Adjust examples/simplest to a single-node version

---
+++ examples/simplest/Simple.hs
@@ -0,0 +1,33 @@
+{-
+ - demonstration of a simple stream-processing program to be
+ - executed on a single node.
+ -}
+
+module Simple where
+
+import Striot.FunctionalIoTtypes
+import Striot.FunctionalProcessing
+import Striot.Nodes
+
+import Control.Concurrent (threadDelay)
+import System.Random (getStdRandom, randomR)
+import Data.Function ((&))
+
+-- periodically generates a random Int
+streamSrc :: IO Int
+streamSrc = do
+    i <- getStdRandom (randomR (1,10)) :: IO Int
+    threadDelay (1000*1000)
+    return i
+
+streamSink :: Stream Int -> IO ()
+streamSink = mapM_ print
+
+-- a simple pure stream-processing program
+streamFn :: Stream Int -> Stream Int
+streamFn = streamFilter (<15)
+         . streamMap (*2)
+
+-- plumb the stream-processing program together with the
+-- source and sink functions, on a single node.
+main = nodeSimple streamSrc streamFn streamSink
