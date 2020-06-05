c0955f0acf8ff21d78b36db39a80bbb4fc888f3f
Jonathan Dowland <jon@dow.land>
2020-06-05 16:02:28 +0100

    update VizGraph for StreamVertex changes

--- src/Striot/VizGraph.hs
+++ src/Striot/VizGraph.hs
@@ -10,6 +10,7 @@ import Algebra.Graph.Export.Dot
 import Data.String
 import Test.Framework
 import Data.List (intercalate)
+import Language.Haskell.TH
 
 import System.Process
 import System.IO (openTempFile, hPutStr, hClose)
@@ -18,7 +19,7 @@ streamGraphToDot :: StreamGraph -> String
 streamGraphToDot = export myStyle
 
 show' :: StreamVertex -> String
-show' v = intercalate " " $ ((printOp . operator) v) : ((map (\s->"("++s++")")) . parameters) v
+show' v = intercalate " " $ ((printOp . operator) v) : ((map (\s->"("++s++")")) . map showParam . parameters) v
 
 printOp :: StreamOperator -> String
 printOp = (++) "stream" . show
@@ -40,21 +41,27 @@ escape [] = []
 escape (x:xs) = if x == '"' then '\\':'"':(escape xs) else x:(escape xs)
 
 -- test data
---source x = "do\n    threadDelay (1000*1000)\n    putStrLn \"sending '"++x++"'\"\n    return \""++x++"\""
-source x = "do threadDelay (1000*1000); putStrLn \"sending '"++x++"'\"; return \""++x++"\""
-v1 = StreamVertex 1 Source [source "foo"]  "String" "String"
-v2 = StreamVertex 2 Map    ["Prelude.id"]  "String" "String"
-v3 = StreamVertex 3 Source [source "bar"]  "String" "String"
-v4 = StreamVertex 4 Map    ["Prelude.id"]  "String" "String"
-v5 = StreamVertex 5 Merge  ["[n1,n2]"]     "[String]" "String"
-v6 = StreamVertex 6 Sink   ["mapM_ print"] "String" "IO ()"
+
+source x = [| do
+    let x' = $(litE (StringL x))
+    threadDelay (1000*1000)
+    putStrLn "sending '"++x'++"'"
+    return x'
+    |]
+
+v1 = StreamVertex 1 Source [source "foo"]    "String" "String"
+v2 = StreamVertex 2 Map    [[| id |]]        "String" "String"
+v3 = StreamVertex 3 Source [source "bar"]    "String" "String"
+v4 = StreamVertex 4 Map    [[| id |]]        "String" "String"
+v5 = StreamVertex 5 Merge  []                "[String]" "String"
+v6 = StreamVertex 6 Sink   [[| mapM_ print|]] "String" "IO ()"
 mergeEx :: StreamGraph
 mergeEx = overlay (path [v3, v4, v5]) (path [v1, v2, v5, v6])
 
-v7 = StreamVertex 1 Source ["<source of random tweets>"] "String" "String"
-v8 = StreamVertex 2 Map    ["filter (('#'==).head) . words"] "String" "[String]"
-v9 = StreamVertex 5 Expand [""]                 "[String]" "String"
-v10 = StreamVertex 6 Sink   ["mapM_ print"] "String" "IO ()"
+v7 = StreamVertex  1 Source [[| sourceOfRandomTweets |]] "String" "String"
+v8 = StreamVertex  2 Map    [[| filter (('#'==).head) . words |]] "String" "[String]"
+v9 = StreamVertex  5 Expand [] "[String]" "String"
+v10 = StreamVertex 6 Sink   [[|mapM_ print|]] "String" "IO ()"
 expandEx :: StreamGraph
 expandEx = path [v7, v8, v9, v10]
 
