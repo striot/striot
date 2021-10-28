e38b921294cb7d7dd71fad7761b55ff8084a7162
Jonathan Dowland <jon@dow.land>
2021-10-28 16:29:48 +0100

    implement Rejecting over-utilised Partitions

--- src/Striot/Orchestration.hs
+++ src/Striot/Orchestration.hs
@@ -28,7 +28,7 @@ module Striot.Orchestration ( distributeProgram
                             ) where
 
 import Algebra.Graph
-import Data.List (nub, sortOn)
+import Data.List (nub, sortOn, sort)
 import Data.Maybe (fromJust, isJust)
 import Test.Framework
 import Data.Function ((&))
@@ -93,6 +93,9 @@ test_viableRewrites_tooMuch = assertEmpty  $ viableRewrites tooMuch
 toSnd :: (a -> b) -> a -> (a, b)
 toSnd f a = (a, f a)
 
+toFst :: (a -> b) -> a -> (b, a)
+toFst f a = (f a, a)
+
 -- | given a StreamGraph, derives further graphs by applying rewrite
 -- rules and pairs them with all their potential partitionings
 deriveRewritesAndPartitionings :: StreamGraph -> [(StreamGraph,PartitionMap)]
@@ -105,17 +108,22 @@ allPartitionsPaired sg = map (\pm -> (sg,pm)) (allPartitions sg)
 
 -- | Return a score for a 'StreamGraph'/'PartitionMap' pair representing a
 -- "cost" for the pair, where lower is better. The score is wrapped in
--- a 'Maybe'. 'sumUtility' returns Nothing if the pairing is not viable.
+-- a 'Maybe'. 'sumUtility' returns Nothing if the pairing is not viable,
+-- either due to an over-utilised operator or an over-utilised Partition.
 --
--- TODO what about no viable partitionings?? Can this happen?
 sumUtility :: StreamGraph -> PartitionMap -> Score
 sumUtility sg pm = let
     oi = calcAllSg sg
     in if isOverUtilised oi
        then Nothing
-       else let graphScore = sum . map util $ oi
-                partScore  = fromIntegral (length pm)
-                in Just    $ (graphScore, partScore)
+
+       -- filter our Partition over-utilisation
+       else if   any (>partitionUtilThreshold) (sumUtilPartitions oi pm)
+            then Nothing
+
+            else let graphScore = sum . map util $ oi
+                     partScore  = fromIntegral (length pm)
+                     in Just    $ (graphScore, partScore)
 
 -- | fitness function for StreamGraphs. Is this a viable program?
 viableStreamGraph :: [OperatorInfo] -> Bool
@@ -152,3 +160,48 @@ tooMuch = simpleStream
 test_tooMuch_notviable = assertBool (not . viableStreamGraph . calcAllSg $ tooMuch)
 
 main = htfMain htf_thisModulesTests
+
+------------------------------------------------------------------------------
+-- Rejecting over-utilised Partitions
+
+partitionUtilThreshold = 3.0 -- finger in the air
+
+sumUtilPartitions :: [OperatorInfo] -> PartitionMap -> [Double]
+sumUtilPartitions oi pm =
+    let oi' = map (toFst opId) oi -- :: [(Int, OperatorInfo)]
+    in map (sumUtilPartition oi') pm
+
+sumUtilPartition :: [(Int, OperatorInfo)] -> [Partition] -> Double
+sumUtilPartition opInfo =
+        sum . map (util . fromJust . (flip lookup) opInfo)
+
+partUtilGraph = simpleStream
+    [ ( Source 1, [[| tempSensor  |]], "Int",    1 )
+    , ( Map,      [[| farToCels   |]], "Int",    1 )
+    , ( Filter 1, [[| over100     |]], "Int",    1 )
+
+    , ( Map,      [[| farToCels   |]], "Int",    1 )
+    , ( Filter 1, [[| over100     |]], "Int",    1 )
+    , ( Map,      [[| farToCels   |]], "Int",    1 )
+
+    , ( Filter 1, [[| over100     |]], "Int",    1 )
+    , ( Map,      [[| farToCels   |]], "Int",    1 )
+    , ( Sink,     [[| mapM_ print |]], "IO ()",  1 )
+    ]
+
+-- when partition over-utilisation is not considered, the cost model would
+-- prefer a PartitionMap with the fewest partitions, which would be 2.
+-- When Partition utilisation is capped by partitionUtilThreshold, the
+-- minimum number of partitions of partUtilGraph is 3.
+
+-- expensive to evaluate (1911ms)
+test_overUtilisedPartition_minThreePartitions = assertBool $
+    (not . any (<3) . map (length.snd.fst) . viableRewrites) partUtilGraph
+
+test_overUtilisedPartition_rejected = -- example of an over-utilised partition
+    assertNothing (sumUtility partUtilGraph [[1,2],[3,4,5,6,7,8,9]])
+
+-- example of an acceptable PartitionMap
+test_overUtilisedPartition_acceptable = assertElem [[1,2,3],[4,5,6],[7,8,9]]
+    $ map (sort . (map sort))
+    $ (map (snd.fst) . viableRewrites) partUtilGraph -- :: [PartitionMap]
