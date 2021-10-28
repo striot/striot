8b4bda3641970ff8553397aa356ab41a2144525d
Jonathan Dowland <jon@dow.land>
2021-10-28 14:10:31 +0100

    CompileIoT: export Partition type

--- src/Striot/CompileIoT.hs
+++ src/Striot/CompileIoT.hs
@@ -6,6 +6,7 @@ module Striot.CompileIoT ( createPartitions
                          , generateCode
                          , GenerateOpts(..)
                          , defaultOpts
+                         , Partition
                          , PartitionMap
                          , writePart
                          , genDockerfile
