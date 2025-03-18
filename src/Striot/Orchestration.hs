bc21c93411d7727121e4449d80aeffe8c670e9a6
Jonathan Dowland <jon@dow.land>
2025-03-18 14:57:29 +0000

    Orchestration: export CostedPlan type

--- src/Striot/Orchestration.hs
+++ src/Striot/Orchestration.hs
@@ -14,6 +14,7 @@ programs described as 'StreamGraph's.
 
 -}
 module Striot.Orchestration ( Cost
+                            , CostedPlan(..)
                             , distributeProgram
                             , chopAndChange
                             , viableRewrites
